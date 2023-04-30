aea91eea3099286a7827fa73edddb935f7eafcc4
John DH. Park <john.dh.park@me.com>
2023-04-30 10:59:06 -0700

    completed context-lab

--- index.js
+++ index.js
@@ -9,15 +9,110 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+function createEmployeeRecord(employee) {
+    const [firstName, familyName, title, payPerHour] = employee;
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
 }
 
+function createEmployeeRecords(employees) {
+    return employees.map((employee) => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+
+    const event = {
+        type: "TimeIn",
+        date,
+        hour: parseInt(hour),
+    };
+
+    this.timeInEvents.push(event);
+
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+
+    const event = {
+        type: "TimeOut",
+        date,
+        hour: parseInt(hour),
+    };
+
+    this.timeOutEvents.push(event);
+
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const { timeInEvents, timeOutEvents } = this;
+    const len = timeInEvents.length;
+
+    let totHrs = 0;
+
+    for (let i = 0; i < len; ++i) {
+        const timeInEvent = timeInEvents[i];
+        const timeOutEvent = timeOutEvents[i];
+
+        if (timeInEvent.date != date) continue;
+
+        totHrs = timeOutEvent.hour - timeInEvent.hour;
+        break;
+    }
+
+    totHrs /= 100;
+    return totHrs;
+}
+
+function wagesEarnedOnDate(date) {
+    const { payPerHour } = this;
+
+    return hoursWorkedOnDate.call(this, date) * payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find((employee) => employee.firstName === firstName);
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce((acc, employee, index) => {
+        const { timeInEvents, timeOutEvents, payPerHour } = employee;
+        const len = employee.timeInEvents.length;
+
+        let totHrs = 0;
+
+        for (let i = 0; i < len; ++i) {
+            const timeInEvent = timeInEvents[i];
+            const timeOutEvent = timeOutEvents[i];
+
+            totHrs += (timeOutEvent.hour - timeInEvent.hour) / 100;
+        }
+
+        return acc + totHrs * payPerHour;
+    }, 0);
+}
+
+const allWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date;
+    });
+
+    const payable = eligibleDates.reduce(
+        function (memo, d) {
+            return memo + wagesEarnedOnDate.call(this, d);
+        }.bind(this),
+        0
+    ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+    return payable;
+};
